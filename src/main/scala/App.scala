ed5c07f89608ae0eef53e6e71331f37a227a13be
anthony <antonsdl@gmail.com>
2018-10-27 00:57:50 +0300

    added simple functional filter and rem-dups

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -11,15 +11,55 @@ object App {
     var x = 5
     var y = if (0 == x % 2) 1 else 0
 
-    print(y)
+    println(y)
 
+    val xs = List(1, 2, 3, 4, 5)
+    val pred: Int => Boolean =
+      x => 0 == x % 2
 
-//    val fact: Int => Int
-//        fact
-//      x => if (0 == x) 1 else 1 * fact(x - 1)
+    val multiplier: Int => Double =
+      x => x * 2.5
 
-//    print(s"${fact(5)}")
+    val ys = filter(pred, multiplier, xs)
+    println(ys)
 
+    val fs = List(1, 2, 3, 2, 3, 4, 5)
+    println(removeDups(fs))
+  }
 
+  def filter(pred: Int => Boolean,
+             func: Int => Double,
+             xs: List[Int]): List[Double] = {
+    if (xs.isEmpty) {
+      List.empty
+
+    } else {
+      val head :: tail = xs
+      if (pred(head)) {
+        List(func(head)) ::: filter(pred, func, tail)
+
+      } else {
+        filter(pred, func, tail)
+      }
+    }
+  }
+
+  def removeDups(xs: List[Int]): List[Int] = {
+    dupHeler(List.empty, xs)
+  }
+
+  def dupHeler(noDups: List[Int], xs: List[Int]): List[Int] = {
+    if (xs.isEmpty) {
+      noDups
+
+    } else {
+      val head :: tail = xs
+      if (noDups.contains(head)) {
+        dupHeler(noDups, tail)
+
+      } else {
+        dupHeler(noDups ++ List(head), tail)
+      }
+    }
   }
 }

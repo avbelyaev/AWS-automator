9ba2275ad63739aaa3d6a60e86cd9620ef975bca
anthony <antonsdl@gmail.com>
2018-11-11 00:09:56 +0300

    removed descriptor

--- src/main/scala/ru/belyaev/automata/domain/model/resource/RaxInstance.scala
+++ src/main/scala/ru/belyaev/automata/domain/model/resource/RaxInstance.scala
@@ -1,20 +1,20 @@
 package ru.belyaev.automata.domain.model.resource
 
+import com.amazonaws.services.ec2.model.Instance
 import org.joda.time.DateTime
 
 /**
   * @author avbelyaev
   */
-class RaxInstance(descriptor: Descriptor)
+class RaxInstance(descriptor: Instance)
   extends CloudResource {
 
-  val resourceType: String = descriptor.resourceType
-  val state: String = descriptor.state
-  val ip: String = descriptor.ip
+  val launchTime: DateTime = DateTime.now()
+  val resourceType: String = "t2"
+  val state: String = "down"
+  val name: String = "rax-no-name"
+  val ip: String = "8.8.8.8"
 
   override def toString: String =
     s"RaxInstance ${super.toString()}, type ${this.resourceType}"
-
-  override val launchTime: DateTime = DateTime.now()
-  override val name: String = "rax-no-name"
 }

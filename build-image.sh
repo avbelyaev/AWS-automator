8155ec8d9a7a7db6af19b5cec8204676dd2c4e38
anthony <antonsdl@gmail.com>
2018-11-11 03:16:57 +0300

    app now can be built into fat jar

---
+++ build-image.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+tag_name=scala-autmator
+
+echo "Assembling fat jar"
+sbt assembly
+
+echo "Building image"
+docker build --tag ${tag_name} .
+
+echo "Pushing image"
+#docker push ${tag_name}
+
+echo "Image has been successfully pushed"

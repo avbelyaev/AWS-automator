e75ffefa7362d4bb24e0a578e0e9bb4c6d9d11c6
Anton Belyaev <anton.belyaev@bostongene.com>
2018-11-16 19:38:57 +0300

    added logging

--- build.sbt
+++ build.sbt
@@ -4,9 +4,11 @@ version := "0.1"
 
 scalaVersion := "2.12.7"
 
+libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.2.3"
 libraryDependencies += "com.amazonaws" % "aws-java-sdk-ec2" % "1.11.446"
 libraryDependencies += "com.github.nscala-time" %% "nscala-time" % "2.20.0"
 libraryDependencies += "com.typesafe" % "config" % "1.3.2"
+libraryDependencies += "com.typesafe.scala-logging" %% "scala-logging" % "3.9.0"
 libraryDependencies += "org.apache.jclouds" % "jclouds-core" % "2.1.1"
 libraryDependencies += "org.apache.jclouds.api" % "openstack-nova" % "2.1.1"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
--- src/main/scala/ru/belyaev/automata/CloudHostAutomator.scala
+++ src/main/scala/ru/belyaev/automata/CloudHostAutomator.scala
@@ -1,6 +1,8 @@
 package ru.belyaev.automata
 
 import com.typesafe.config.{Config, ConfigFactory}
+import com.typesafe.scalalogging.{LazyLogging, Logger}
+import org.slf4j.LoggerFactory
 import ru.belyaev.automata.application.{Filter, PrettyPrinter, ResourceChecker}
 import ru.belyaev.automata.domain.model.{AwsApiClient, CloudResource, RaxApiClient}
 
@@ -11,8 +13,11 @@ object CloudHostAutomator {
 
   val conf: Config = ConfigFactory.load()
 
+  val logger = Logger(LoggerFactory.getLogger(this.getClass))
+
+
   def main(arg: Array[String]): Unit = {
-    println("starting")
+    logger.info("Performing runtime threshold check")
 
     val missingResources = awsResources() //.union(raxResources())
 
@@ -30,6 +35,7 @@ object CloudHostAutomator {
     val nameRegex = conf.getString("aws.name-regex")
     val runtimeThreshold = conf.getInt("aws.runtime-threshold-h")
     val filter = new Filter(nameRegex, runtimeThreshold)
+    logger.info(s"Aws config: $filter")
 
     ResourceChecker.missingResources(aws, filter)
   }
@@ -40,6 +46,7 @@ object CloudHostAutomator {
     val nameRegex = conf.getString("rax.name-regex")
     val runtimeThreshold = conf.getInt("rax.runtime-threshold-h")
     val filter = new Filter(nameRegex, runtimeThreshold)
+    logger.info(s"Rackspace config: $filter")
 
     ResourceChecker.missingInstances(rax, filter)
   }
